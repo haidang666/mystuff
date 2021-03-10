4014d8552d03a86ebd9e5964c0e23278674a0944
Hoang Ho Hai Dang <haidang@bacoor.co>
2021-03-10 17:30:51 +0700

    fix: main menu item icon

--- resources/js/Shared/MainMenu/MainMenuItem/index.js
+++ resources/js/Shared/MainMenu/MainMenuItem/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 import classNames from 'classnames';
-import Icon from '@/Shared/Icon';
 import { Icon as IconX } from 'semantic-ui-react';
 
 export default ({ icon, link, text }) => {
   const isActive = route().current(link + '*');
 
-  const iconClasses = classNames('w-4 h-4 mr-2', {
-    'text-white fill-current': isActive,
-    'text-indigo-400 group-hover:text-white fill-current': !isActive
-  });
-
   const textClasses = classNames({
     'text-white': isActive,
     'text-indigo-200 group-hover:text-white': !isActive
   });
-  console.log(isActive);
+
   return (
     <div className="mb-4">
       <InertiaLink href={route(link)} className="flex items-center group py-3">
-        <Icon name={icon} className={iconClasses} />
+        <IconX name={icon} size='large' disabled={!isActive} className={textClasses}/>
         <div className={textClasses}>{text}</div>
-        <IconX name='sticky note' size='large' disabled={!isActive}/>
       </InertiaLink>
     </div>
   );
---
+++ resources/js/Shared/MainMenu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainMenuItem from '@/Shared/MainMenu/MainMenuItem';
+
+export default ({ className }) => {
+  return (
+    <div className={className}>
+      <MainMenuItem text="Dashboard" link="dashboard" icon="dashboard" />
+      <MainMenuItem text="Organizations" link="organizations" icon="building" />
+      <MainMenuItem text="Contacts" link="contacts" icon="address book" />
+      <MainMenuItem text="Reports" link="reports" icon="print" />
+      <MainMenuItem text="Notes" link="reports" icon="sticky note" />
+    </div>
+  );
+};
